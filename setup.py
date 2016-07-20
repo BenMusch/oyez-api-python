d9fcc1a2a8ce0e00ea64f6b2a5463c4ef472ad80
Ben Muschol <muschol.b@husky.neu.edu>
2016-07-19 22:08:09 -0400

    make package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='oyez_api',
+      version='0.1',
+      description='The funniest joke in the world',
+      url='http://github.com/benmusch/oyez-api-python',
+      author='Ben Muschol',
+      author_email='benmuschol@gmail.com',
+      license='MIT',
+      packages=['oyez_api'],
+      zip_safe=False)
